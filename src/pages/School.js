5fd540097c0c55364e4871def501c80212829ed7
Ashton <ashtongabby@gmail.com>
2021-09-26 16:59:53 -0400

    colored titles and new format for schools
    IVE LEARNED TO MUCH

--- src/pages/School.js
+++ src/pages/School.js
@@ -4,315 +4,202 @@ import SchoolInfo from '../components/SchoolInformation';
 function School () {
     return (
         <>
+
         <div>
-            <h1>Schools Page</h1> 
-            
-            <div>
-            <div align= "center">
-                <br/>
-                <h3>Faren</h3>
-                <div>
-                    <h4>Level 1</h4>
-                    <li>Fog</li>
-                    <li>Light</li>
-                    <li>Mystic Touch</li>
-                    <li>Sweep</li>
-                    <li>Zap</li>
-
-                    <h4>Level 2</h4>
-                    <li>Bramble Wall</li>
-                    <li>Icy Fingers</li>
-                    <li>Resist Cold</li>
-                    <li>Touch of Flame</li>
-                    <li>Withstand Fire</li>
-
-                    <h4>Level 3</h4>
-                    <li>Heat</li>
-                    <li>Mana Focus</li>
-                    <li>Resist Shock</li>
-                    <li>Shocking Fury</li>
-                    <li>Spider Web</li>
-                    <li>Winds</li>
-
-                    <h4>Level 4</h4>
-                    <li>Blast of Fire</li>
-                    <li>Brittle</li>
-                    <li>Explosive Frost</li>
-                    <li>Fireball</li>
-                    <li>Firewall</li>
-
-                    <h4>Level 5</h4>
-                    <li>Earthquake</li>
-                    <li>Lightning Bolt</li>
-                    <li>Ring of Flames</li>
-                    <li>Sand Storm</li>
-                    <li>Shatter</li>
-
-                    <h4>Level 6</h4>
-                    <li>Lightning Wall</li>
-                    <li>Spore Burst</li>
-                </div>
-            </div>
-            <div align= "center">
-                <br/>
-                <br/>
-                <h3>Jala</h3>
-                <div >
-                    <h4>Level 1</h4>
-                    <li>Invigorate</li>
-                    <li>Melancholy</li>
-                    <li>Mirth</li>
-
-                    <h4>Level 2</h4>
-                    <li>Civility</li>
-                    <li>Concilation</li>
-                    <li>Warp Time</li>
-
-                    <h4>Level 3</h4>
-                    <li>Distill</li>
-                    <li>Profane Resonance</li>
-                    <li>Restorate</li>
-                    <li>Sacred Resonance</li>
-
-                    <h4>Level 4</h4>
-                    <li>Truth</li>
-                    <li>Disharmony</li>
-                    <li>Jig</li>
-
-                    <h4>Level 5</h4>
-                    <li>Spellbane</li>
-                    <li>Mana Convergence</li>
-                    <li>Rejuvenate</li>
-
-                    <h4>Level 6</h4>
-                    <li>Crystalize Mana</li>
-
-                </div>
-            </div>
-            <div align= "center">
-                <br/>
-                <br/>
-                <h3>Kraanan</h3>
-                <div>
-                    
-                <h4>Level 1</h4>
-                    <li>Create Food</li>
-                    <li>Create Weapon</li>
-                    <li>Glow</li>
-                    <li>Relay</li>
-
-                    <h4>Level 2</h4>
-                    <li>Bless</li>
-                    <li>Enchant Weapon</li>
-                    <li>Haste</li>
-                    <li>Resist Poison</li>
-                    <li>Super Strength</li>
-
-                    <h4>Level 3</h4>
-                    <li>Detect Invisible</li>
-                    <li>Free Action</li>
-                    <li>Magic Shield</li>
-                    <li>Martyr's Battleground</li>
-                    <li>Night Vision</li>
-                    <li>Shroud</li>
-
-                    <h4>Level 4</h4>
-                    <li>Deflect</li>
-                    <li>Discordance</li>
-                    <li>Dispel Illusion</li>
-                    <li>Eagle Eyes</li>
-                    <li>Mana Bomb</li>
-                    <li>Mend</li>
-
-                    <h4>Level 5</h4>
-                    <li>Armor of Gort</li>
-                    <li>Hunt</li>
-                    <li>Killing Fields</li>
-                    <li>Resist Magic</li>
-
-                    <h4>Level 6</h4>
-                    <li>Anti-Magic Aura</li>
-                    <li>Shatterlock</li>
-                
-                </div>
-            </div>
-            <div align= "center">
-                <br/>
-                <br/>
-                <h3>Qor</h3>
-                <div>
-                    
-                    <h4>Level 1</h4>
-                    <li>Cloak</li>
-                    <li>Darkness</li>
-                    <li>Detect Good</li>
-                    <li>Unholy Resolve</li>
-                    <li>Unholy Touch</li>
-
-                    <h4>Level 2</h4>
-                    <li>Acid Touch</li>
-                    <li>Fade</li>
-                    <li>Kara'hol's Curse</li>
-                    <li>Unholy Weapon</li>
-
-                    <h4>Level 3</h4>
-                    <li>Defile</li>
-                    <li>Enfeeble</li>
-                    <li>Poison Fog</li>
-                    <li>Silence</li>
-                    <li>Vampiric Drain</li>
-
-                    <h4>Level 4</h4>
-                    <li>Animate</li>
-                    <li>Curse Weapon</li>
-                    <li>Hold</li>
-                    <li>Shadow Rift</li>
-                    <li>Shal'ille Bane</li>
-                    <li>Splash of Acid</li>
-
-                    <h4>Level 5</h4>
-                    <li>Blind</li>
-                    <li>Blood Inheritance</li>
-                    <li>Death Link</li>
-                    <li>Invisibility</li>
-                    <li>Node Burst</li>
-                    <li>Swap</li>
-
-                    <h4>Level 6</h4>
-                    <li>Gaze of the Basilisk</li>
-                    <li>Death Rift</li>
-                </div>
-            </div>
-            <div align= "center">
-                <br/>
-                <br/>
-                <h3>Riija</h3>
-                <div>
-                    <h4>Level 1</h4>
-                    <li>Anonymity</li>
-                    <li>Blink</li>
-                    <li>Feign Death</li>
-                    <li>Flash</li>
-                    <li>Shadow Form</li>
-
-                    <h4>Level 2</h4>
-                    <li>Bait</li>
-                    <li>Eavesdrop</li>
-                    <li>Foresight</li>
-                    <li>Forget</li>
-
-                    <h4>Level 3</h4>
-                    <li>Illusionary Firewall</li>
-                    <li>Illusionary Form</li>
-                    <li>Illusionary Wounds</li>
-                    <li>Vertigo</li>
-
-                    <h4>Level 4</h4>
-                    <li>Apparition</li>
-                    <li>Dement</li>
-                    <li>Denial</li>
-
-                    <h4>Level 5</h4>
-                    <li>Reflection</li>
-                    <li>Artifice</li>
-                    <li>Evil Twin</li>
-                    <li>Morph</li>
-
-                    <h4>Level 6</h4>
-                    <li>Elusion</li>
-
-                </div>
-            </div>
-            <div align= "center">
-                <br/>
-                <br/>
-                <h3>Shal'ille</h3>
-                <div >
-                    
-                    <h4>Level 1</h4>
-                    <li>Breath of Life</li>
-                    <li>Detect Evil</li>
-                    <li>Holy Symbol</li>
-                    <li>Minor Heal</li>
-                    <li>Seance</li>
-
-                    <h4>Level 2</h4>
-                    <li>Cure Poison</li>
-                    <li>Holy Touch</li>
-                    <li>Holy Weapon</li>
-                    <li>Remove Curse</li>
-                    <li>Resist Acid</li>
-
-                    <h4>Level 3</h4>
-                    <li>Cure Disease</li>
-                    <li>Holy Resolve</li>
-                    <li>Hospice</li>
-                    <li>Identify</li>
-                    <li>Rescue</li>
-                    <li>Spiritual Hammer</li>
-
-                    <h4>Level 4</h4>
-                    <li>Dazzle</li>
-                    <li>Forces of Light</li>
-                    <li>Mark of Dishonor</li>
-                    <li>Purify</li>
-                    <li>Qorbance</li>
-                    <li>Truce</li>
-
-                    <h4>Level 5</h4>
-                    <li>Final Rites</li>
-                    <li>Major Heal</li>
-                    <li>Purge</li>
-                    <li>Reveal</li>
-                    <li>Umbrella</li>
-
-                    <h4>Level 6</h4>
-                    <li>Bond</li>
-                    <li>Portal of Life</li>
-                    
-                </div>
-            </div>
-            <div align= "center">
-                <br/>
-                <br/>
-                <h3>Weaponcraft</h3>
-                <div>
-                    
-                    <h4>Level 1</h4>
-                    <li>Block</li>
-                    <li>Mace Fighting</li>
-                    <li>Punch</li>
-                    <li>Slash</li>
-
-                    <h4>Level 2</h4>
-                    <li>Brawling</li>
-                    <li>Dodge</li>
-                    <li>Short Sword Fighting</li>
-
-                    <h4>Level 3</h4>
-                    <li>Axe Wielding</li>
-                    <li>Fencing</li>
-                    <li>Hammer Wielding</li>
-
-                    <h4>Level 4</h4>
-                    <li>Fire</li>
-                    <li>Parry</li>
-                    <li>Scimitar Wielding</li>
-                    <li>Second Wind</li>
-
-                    <h4>Level 5</h4>
-                    <li>Archery</li>
-                    <li>Cleave</li>
-
-                    <h4>Level 6</h4>
-                    <li>Disarm</li>
-                    
-                </div>
-            </div>
-            </div>
-            <div>
-                <SchoolInfo/>
-            </div>
+            <center>
+            <faren>Faren</faren>
+        <table  width="45%" border="1">
+            <tr class="title">
+                <td  height="19">
+                <h2>Level</h2>
+                </td>
+                <td  height="19">
+                <h2>Spell</h2>
+                </td>
+                <td  height="19">
+                <h2>Seller</h2>
+                </td>
+            </tr>
+            <tr class="detail1">
+                <td  height="19">Level 1</td>
+                <td  height="19">Light</td>
+                <td  height="19">10</td>
+            </tr>
+            <tr class="detail2">
+                <td  height="19"></td>
+                <td  height="19">Fog</td>
+                <td  height="19">10</td>
+            </tr>
+            </table>
+            </center>
+        </div>
+        <div>
+            <center>
+            <jala>Jala</jala>
+        <table  width="45%" border="1">
+            <tr class="title">
+                <td  height="19">
+                <h2>Level</h2>
+                </td>
+                <td  height="19">
+                <h2>Spell</h2>
+                </td>
+                <td  height="19">
+                <h2>Seller</h2>
+                </td>
+            </tr>
+            <tr class="detail1">
+                <td  height="19">Level 1</td>
+                <td  height="19">Light</td>
+                <td  height="19">10</td>
+            </tr>
+            <tr class="detail2">
+                <td  height="19"></td>
+                <td  height="19">Fog</td>
+                <td  height="19">10</td>
+            </tr>
+            </table>
+            </center>
+        </div>
+        <div>
+            <center>
+            <kraanan>Kraanan</kraanan>
+        <table  width="45%" border="1">
+            <tr class="title">
+                <td  height="19">
+                <h2>Level</h2>
+                </td>
+                <td  height="19">
+                <h2>Spell</h2>
+                </td>
+                <td  height="19">
+                <h2>Seller</h2>
+                </td>
+            </tr>
+            <tr class="detail1">
+                <td  height="19">Level 1</td>
+                <td  height="19">Light</td>
+                <td  height="19">10</td>
+            </tr>
+            <tr class="detail2">
+                <td  height="19"></td>
+                <td  height="19">Fog</td>
+                <td  height="19">10</td>
+            </tr>
+            </table>
+            </center>
+        </div>
+        <div>
+            <center>
+            <qor>Qor</qor>
+        <table  width="45%" border="1">
+            <tr class="title">
+                <td  height="19">
+                <h2>Level</h2>
+                </td>
+                <td  height="19">
+                <h2>Spell</h2>
+                </td>
+                <td  height="19">
+                <h2>Seller</h2>
+                </td>
+            </tr>
+            <tr class="detail1">
+                <td  height="19">Level 1</td>
+                <td  height="19">Light</td>
+                <td  height="19">10</td>
+            </tr>
+            <tr class="detail2">
+                <td  height="19"></td>
+                <td  height="19">Fog</td>
+                <td  height="19">10</td>
+            </tr>
+            </table>
+            </center>
+        </div>
+        <div>
+            <center>
+            <riija>Riija</riija>
+        <table  width="45%" border="1">
+            <tr class="title">
+                <td  height="19">
+                <h2>Level</h2>
+                </td>
+                <td  height="19">
+                <h2>Spell</h2>
+                </td>
+                <td  height="19">
+                <h2>Seller</h2>
+                </td>
+            </tr>
+            <tr class="detail1">
+                <td  height="19">Level 1</td>
+                <td  height="19">Light</td>
+                <td  height="19">10</td>
+            </tr>
+            <tr class="detail2">
+                <td  height="19"></td>
+                <td  height="19">Fog</td>
+                <td  height="19">10</td>
+            </tr>
+            </table>
+            </center>
+        </div>
+        <div>
+            <center>
+            <shal>Shal'ille</shal>
+        <table  width="45%" border="1">
+            <tr class="title">
+                <td  height="19">
+                <h2>Level</h2>
+                </td>
+                <td  height="19">
+                <h2>Spell</h2>
+                </td>
+                <td  height="19">
+                <h2>Seller</h2>
+                </td>
+            </tr>
+            <tr class="detail1">
+                <td  height="19">Level 1</td>
+                <td  height="19">Light</td>
+                <td  height="19">10</td>
+            </tr>
+            <tr class="detail2">
+                <td  height="19"></td>
+                <td  height="19">Fog</td>
+                <td  height="19">10</td>
+            </tr>
+            </table>
+            </center>
+        </div>
+        <div>
+            <center>
+            <wc>Weaponcraft</wc>
+        <table  width="45%" border="1">
+            <tr class="title">
+                <td  height="19">
+                <h2>Level</h2>
+                </td>
+                <td  height="19">
+                <h2>Spell</h2>
+                </td>
+                <td  height="19">
+                <h2>Seller</h2>
+                </td>
+            </tr>
+            <tr class="detail1">
+                <td  height="19">Level 1</td>
+                <td  height="19">Light</td>
+                <td  height="19">10</td>
+            </tr>
+            <tr class="detail2">
+                <td  height="19"></td>
+                <td  height="19">Fog</td>
+                <td  height="19">10</td>
+            </tr>
+            </table>
+            </center>
         </div>
        
         </>
